4cf4ce11eac12246e4c62c8c51ce09a5476a1d13
KrysPaw <73787119+KrysPaw@users.noreply.github.com>
2020-11-17 10:16:00 +0100

    com2.1
    quest log added

---
+++ Clavis/Clavis/Models/Room.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clavis.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Numer { get; set; }
+        public string Opis { get; set; }
+        public bool Vacant { get; set; }
+    }
+}
